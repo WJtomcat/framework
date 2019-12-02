aabf6dbe291630f2720a670a06f9dd7b1364ca30
wangju <wangju@ainnovation.com>
2019-12-02 13:09:37 +0800

    Initial Commit

---
+++ framework/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  framework
+//
+//  Created by Admin on 2019/12/2.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
